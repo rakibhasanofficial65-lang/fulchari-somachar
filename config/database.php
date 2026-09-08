ac43945d0ce9ab82eefa33feff4756eae91a590c
rakibhasanofficial65-lang <rakibhasanofficial65@gmail.com>
2026-09-09 02:10:35 +0600

    Update database.php

--- config/database.php
+++ config/database.php
@@ -16,8 +16,8 @@ try {
         PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
         PDO::ATTR_EMULATE_PREPARES => false,
 
-        PDO::MYSQL_ATTR_SSL_CA => $caFile,
-        PDO::MYSQL_ATTR_SSL_VERIFY_SERVER_CERT => true,
+        Pdo\Mysql::ATTR_SSL_CA => $caFile,
+        Pdo\Mysql::ATTR_SSL_VERIFY_SERVER_CERT => true,
     ];
 
     $pdo = new PDO(
