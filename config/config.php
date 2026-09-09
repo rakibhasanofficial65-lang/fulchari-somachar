888853e2baaaeb6e64ad25fb7c834bbe43ea29ba
rakibhasanofficial65-lang <rakibhasanofficial65@gmail.com>
2026-09-09 13:08:23 +0600

    Update config.php

--- config/config.php
+++ config/config.php
@@ -1,4 +1,33 @@
 <?php
 
 define("SITE_NAME", "ফুলছড়ি সমাচার");
-define("SITE_URL", "http://localhost/fulchari-somachar");
+
+/*
+|--------------------------------------------------------------------------
+| Site URL
+|--------------------------------------------------------------------------
+| Localhost হলে:
+| http://localhost/fulchari-somachar
+|
+| Vercel হলে:
+| Vercel-এর production URL automatically নেওয়া হবে।
+|--------------------------------------------------------------------------
+*/
+
+$vercelProductionUrl = getenv("VERCEL_PROJECT_PRODUCTION_URL");
+$vercelUrl = getenv("VERCEL_URL");
+
+if (!empty($vercelProductionUrl)) {
+
+    $siteUrl = "https://" . $vercelProductionUrl;
+
+} elseif (!empty($vercelUrl)) {
+
+    $siteUrl = "https://" . $vercelUrl;
+
+} else {
+
+    $siteUrl = "http://localhost/fulchari-somachar";
+}
+
+define("SITE_URL", rtrim($siteUrl, "/"));
